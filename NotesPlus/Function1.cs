df04183c72ec242bf6c7091b36389a2a0f6f3ff8
Javier Rivera <ingjrz@gmail.com>
2021-07-15 18:39:55 -0500

    Binding to CosmosDB
    Binding to CosmosDB

--- NotesPlus/Function1.cs
+++ NotesPlus/Function1.cs
@@ -18,30 +18,32 @@ namespace NotesPlus
     {
         [FunctionName("GetNoteById")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
-        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        //[OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
-        [OpenApiParameter(name: "id", In = ParameterLocation.Query, Required = true, Type = typeof(Guid), Description = "The **ID** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]        
+        [OpenApiParameter(name: "id", In = ParameterLocation.Path, Required = true, Type = typeof(Guid), Description = "The **ID** parameter")]
+        [OpenApiParameter(name: "category", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The **Category** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Models.File), Description = "The OK response")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "GetNoteById/{id}")] HttpRequest req, //$"{id}"
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "GetNoteById/{category}/{id}")] HttpRequest req, 
+            [CosmosDB(databaseName: "%Database:Name%", collectionName: "%Database:Collection%", ConnectionStringSetting = "ConnectionStrings:CosmosDb", Id = "{id}", PartitionKey = "{category}")]
+            Models.File file,
+            ILogger log)        
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            //log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string idUnparsed = req.Query["id"];
+            //string idUnparsed = req.Query["id"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            var id = Guid.Parse(idUnparsed);
-            //name = name ?? data?.name;
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //var id = Guid.Parse(idUnparsed);
+            ////name = name ?? data?.name;
 
             //string responseMessage = string.IsNullOrEmpty(name)
-            string responseMessage = string.IsNullOrEmpty(id.ToString())
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                //: $"Hello, {id}. This HTTP triggered function executed successfully.";
-                : $"{id}.";
+            //string responseMessage = string.IsNullOrEmpty(id.ToString())
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    //: $"Hello, {id}. This HTTP triggered function executed successfully.";
+            //    : $"{id}.";
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(file);
         }
     }
 }
---
+++ NotesPlus/Models/File.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesPlus.Models
+{
+    public class File
+    {
+        public Guid Id { get; set;}
+        public string Category { get; set; }
+        public string Content { get; set; }
+    }
+}
